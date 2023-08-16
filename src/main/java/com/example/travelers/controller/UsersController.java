32aec9c02d6ed47fcba4954927d73a50ff0d1229
dothur <craz1000@naver.com>
2023-08-16 11:23:11 +0900

    refactor: Class Rename & UsersService DI

--- src/main/java/com/example/travelers/controller/UsersController.java
+++ src/main/java/com/example/travelers/controller/UsersController.java
@@ -1,5 +1,6 @@
 package com.example.travelers.controller;
 
+import com.example.travelers.service.UsersService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/users")
-public class UserController {
+public class UsersController {
+    private final UsersService usersService;
 
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
 }
