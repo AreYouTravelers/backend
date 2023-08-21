b69c2c0a89496cae6255d90af6bcd2be66dbad36
dothur <craz1000@naver.com>
2023-08-21 15:26:22 +0900

    Feat: 사용자 탈퇴 / 탈퇴처리 용 Dto 추가

---
+++ src/main/java/com/example/travelers/dto/DeleteUserByAdminDto.java
@@ -0,0 +1,11 @@
+package com.example.travelers.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteUserByAdminDto {
+    private String username;
+}
