5f097fa58589de569a1f328167917d91eed6aec1
ycyeon <ickite@naver.com>
2023-08-24 21:52:02 +0900

    Fix: SenderRequestsController endpoint Restful하게 수정

--- src/main/java/com/example/travelers/controller/ReceiverRequestsController.java
+++ src/main/java/com/example/travelers/controller/ReceiverRequestsController.java
@@ -1,5 +1,6 @@
 package com.example.travelers.controller;
 
+import com.example.travelers.dto.MessageResponseDto;
 import com.example.travelers.service.ReceiverRequestsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
