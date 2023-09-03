048df61ce050bccac1022c93bae01f4d7bf06a23
se-ize <seizenim@gmail.com>
2023-09-03 21:22:45 +0900

    Refactor: Redis Cache 설정

--- src/main/java/com/example/travelers/TravelersApplication.java
+++ src/main/java/com/example/travelers/TravelersApplication.java
@@ -2,8 +2,10 @@ package com.example.travelers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TravelersApplication {
 
 	public static void main(String[] args) {
