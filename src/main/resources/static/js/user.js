8104d5d0f7a823036446fce85dc7f2c6508f8ac5
dothur <craz1000@naver.com>
2023-09-09 22:39:37 +0900

    fix: alert 추가, 에러시 catch 문 추가

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,16 +1,21 @@
 const getJwt = () => localStorage.getItem('accessToken')
 
 const fetchLoggedIn = function () {
-    if (!getJwt())
+    if (!getJwt())  // 토큰이 없으면 로그인하지 않은 상태로 처리
         return new Promise(resolve => {
             resolve(false)
         })
-    else return fetch("/users/is-logged-in", {
+    else return fetch("/users/my-profile", {
         headers: {
             'Authorization': `Bearer ${getJwt()}`
         }
-    }).then(response => {
-        if (response.ok) return response.json()
-        else return false
     })
+        .then(response => {
+        if (response.ok) return response.json()
+        else return false  // 로그인 상태가 아님
+    })
+        .catch(error => {
+            console.error(error);
+            return false; // 오류 발생시 로그인 상태가 아님
+        })
 }
