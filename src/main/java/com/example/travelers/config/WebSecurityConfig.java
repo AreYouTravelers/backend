1a268f5fbbe3a8b30f620cb7e3a264751713cf8f
dothur <craz1000@naver.com>
2023-09-13 20:48:37 +0900

    feat: admin 페이지 구현 완료 - 회원 리스트 페이지별로 구현 - 회원 탈퇴 버튼 기능 구현

--- src/main/java/com/example/travelers/config/WebSecurityConfig.java
+++ src/main/java/com/example/travelers/config/WebSecurityConfig.java
@@ -43,6 +43,7 @@ public class WebSecurityConfig {
                                         "/users",
                                         "/login",
                                         "/signup",
+                                        "/admin",
                                         "/mypage",
                                         "/account",
                                         "/accompany",
