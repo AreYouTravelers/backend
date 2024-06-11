8c3597a2e88d3518924da5ad827ed4e4138f4282
yezy <iyeahji@gmail.com>
2024-06-11 17:27:06 +0900

    Feat: 유저 신고하기 (BE, FE 완료)

--- src/main/java/com/example/domain/reports/service/ReportsService.java
+++ src/main/java/com/example/domain/reports/service/ReportsService.java
@@ -1,96 +1,95 @@
-//package com.example.domain.reports.service;
-//
-//import com.example.domain.users.dto.MessageResponseDto;
-//import com.example.domain.reports.dto.ReportsDto;
-//import com.example.domain.reports.domain.Reports;
-//import com.example.domain.users.service.AuthService;
-//import com.example.domain.users.domain.Users;
-//import com.example.domain.reports.repository.ReportsRepository;
-//import com.example.domain.users.repository.UsersRepository;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.data.domain.*;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import java.time.LocalDateTime;
-//import java.util.*;
-//
-//@Slf4j
-//@Service
-//@RequiredArgsConstructor
-//public class ReportsService {
-//    private final ReportsRepository reportsRepository;
-//    private final UsersRepository usersRepository;
-//    private final AuthService authService;
-//
-//    public ReportsDto createReport(ReportsDto dto) {
-//
-//        Users reportedUser = usersRepository.findByUsername(dto.getReportedUser())
-//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "ReportedUser not found"));
-//
-//        Users userEntity = authService.getUser();
-//            Reports newReport = Reports.builder()
-//                    .title(dto.getTitle())
-//                    .content(dto.getContent())
-//                    .reportedUser(dto.getReportedUser())
-//                    .status(false)
-//                    .createdAt(LocalDateTime.now())
-//                    .user(userEntity).build();
-//
-//            Reports savedReport = reportsRepository.save(newReport);
-//            dto.setId(savedReport.getId());
-//            return dto;
-//    }
-//
-//    public ReportsDto readReport(Long id) {
-//        Users userEntity = authService.getUser();
-//        Optional<Reports> report = reportsRepository.findById(id);
-//        if (report.isPresent()) {
-//            if (report.get().getUser().getId().equals(userEntity.getId())
-//                    || userEntity.getRole().equals("관리자")) {
-//                ReportsDto dto = ReportsDto.fromEntity(report.get());
-//                return dto;
-//            } else throw new ResponseStatusException(HttpStatus.FORBIDDEN, "권한이 없습니다.");
-//        } else throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Report not found");
-//    }
-//
-//    public Page<ReportsDto> readReportsAll(Integer pageNumber) {
-//        Users userEntity = authService.getUser();
-//        Pageable pageable = PageRequest.of(pageNumber, 25, Sort.by("id").ascending());
-//        if (userEntity.getRole().equals("관리자")) {
-//            return reportsRepository.findAll(pageable).map(ReportsDto::fromEntity);
-//        } else throw new ResponseStatusException(HttpStatus.FORBIDDEN, "권한이 없습니다.");
-//    }
-//
-//
-//    public MessageResponseDto updateReport(Long id, ReportsDto dto) {
-//        Users userEntity = authService.getUser();
-//        Optional<Reports> report = reportsRepository.findById(id);
-//        if (report.isPresent()) {
-//            if (report.get().getUser().getId().equals(userEntity.getId())) {
-//                Reports reportsEntity = report.get();
-//                reportsEntity.setTitle(dto.getTitle());
-//                reportsEntity.setContent(dto.getContent());
-//                reportsEntity.setReportedUser(dto.getReportedUser());
-//                reportsEntity.setCreatedAt(LocalDateTime.now());
-//                reportsRepository.save(reportsEntity);
-//                return new MessageResponseDto("유저 신고를 업데이트 했습니다.");
-//            } else throw new ResponseStatusException(HttpStatus.FORBIDDEN, "권한이 없습니다.");
-//        } else throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Report not found");
-//    }
-//
-//    public MessageResponseDto deleteReport(Long id) {
-//        Users userEntity = authService.getUser();
-//        Optional<Reports> report = reportsRepository.findById(id);
-//        if (report.isPresent()) {
-//            if (report.get().getUser().getId().equals(userEntity.getId())
-//                    || userEntity.getRole().equals("관리자")) {
-//                Reports reportsEntity = report.get();
-//                reportsRepository.delete(reportsEntity);
-//                return new MessageResponseDto("유저 신고를 삭제했습니다.");
-//            } else throw new ResponseStatusException(HttpStatus.FORBIDDEN, "권한이 없습니다.");
-//        } else throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Report not found");
-//    }
-//}
+package com.example.domain.reports.service;
+
+import com.example.domain.users.dto.MessageResponseDto;
+import com.example.domain.reports.dto.ReportsDto;
+import com.example.domain.reports.domain.Reports;
+import com.example.domain.users.service.AuthService;
+import com.example.domain.users.domain.Users;
+import com.example.domain.reports.repository.ReportsRepository;
+import com.example.domain.users.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ReportsService {
+    private final ReportsRepository reportsRepository;
+    private final UsersRepository usersRepository;
+    private final AuthService authService;
+
+    public ReportsDto createReport(ReportsDto dto) {
+        Users reportedUser = usersRepository.findByUsername(dto.getReportedUser())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "ReportedUser not found"));
+
+        Reports newReport = Reports.builder()
+                .user(authService.getUser())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .reportedUser(dto.getReportedUser())
+                .status(false)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        Reports savedReport = reportsRepository.save(newReport);
+        dto.setId(savedReport.getId());
+        return dto;
+    }
+
+    public ReportsDto readReport(Long id) {
+        Users userEntity = authService.getUser();
+        Optional<Reports> report = reportsRepository.findById(id);
+        if (report.isPresent()) {
+            if (report.get().getUser().getId().equals(userEntity.getId())
+                    || userEntity.getRole().equals("관리자")) {
+                ReportsDto dto = ReportsDto.fromEntity(report.get());
+                return dto;
+            } else throw new ResponseStatusException(HttpStatus.FORBIDDEN, "권한이 없습니다.");
+        } else throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Report not found");
+    }
+
+    public Page<ReportsDto> readReportsAll(Integer pageNumber) {
+        Users userEntity = authService.getUser();
+        Pageable pageable = PageRequest.of(pageNumber, 25, Sort.by("id").ascending());
+        if (userEntity.getRole().equals("관리자")) {
+            return reportsRepository.findAll(pageable).map(ReportsDto::fromEntity);
+        } else throw new ResponseStatusException(HttpStatus.FORBIDDEN, "권한이 없습니다.");
+    }
+
+
+    public MessageResponseDto updateReport(Long id, ReportsDto dto) {
+        Users userEntity = authService.getUser();
+        Optional<Reports> report = reportsRepository.findById(id);
+        if (report.isPresent()) {
+            if (report.get().getUser().getId().equals(userEntity.getId())) {
+                Reports reportsEntity = report.get();
+                reportsEntity.setTitle(dto.getTitle());
+                reportsEntity.setContent(dto.getContent());
+                reportsEntity.setReportedUser(dto.getReportedUser());
+                reportsEntity.setCreatedAt(LocalDateTime.now());
+                reportsRepository.save(reportsEntity);
+                return new MessageResponseDto("유저 신고를 업데이트 했습니다.");
+            } else throw new ResponseStatusException(HttpStatus.FORBIDDEN, "권한이 없습니다.");
+        } else throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Report not found");
+    }
+
+    public MessageResponseDto deleteReport(Long id) {
+        Users userEntity = authService.getUser();
+        Optional<Reports> report = reportsRepository.findById(id);
+        if (report.isPresent()) {
+            if (report.get().getUser().getId().equals(userEntity.getId())
+                    || userEntity.getRole().equals("관리자")) {
+                Reports reportsEntity = report.get();
+                reportsRepository.delete(reportsEntity);
+                return new MessageResponseDto("유저 신고를 삭제했습니다.");
+            } else throw new ResponseStatusException(HttpStatus.FORBIDDEN, "권한이 없습니다.");
+        } else throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Report not found");
+    }
+}
--- src/main/resources/static/js/report-write.js
+++ src/main/resources/static/js/report-write.js
@@ -1,25 +1,9 @@
 // accessToken 불러오기
 let accessToken = localStorage.getItem('accessToken');
 
-function updateCreatedAtField() {
-    var createdAtField = document.getElementById("createdAt");
-    var currentDateTime = new Date();
-    var year = currentDateTime.getFullYear();
-    var month = String(currentDateTime.getMonth() + 1).padStart(2, "0");
-    var day = String(currentDateTime.getDate()).padStart(2, "0");
-    var hours = String(currentDateTime.getHours()).padStart(2, "0");
-    var minutes = String(currentDateTime.getMinutes()).padStart(2, "0");
-    var formattedDateTime = year + "-" + month + "-" + day + " " + hours + ":" + minutes;
-    createdAtField.value = formattedDateTime;
-}
-
 document.addEventListener('DOMContentLoaded', function () {
     const postForm = document.getElementById('postForm');
     const postButton = document.getElementById('postButton');
-
-    updateCreatedAtField()
-    // 작성일 업데이트 간격 설정 (예: 1분마다 업데이트)
-    setInterval(updateCreatedAtField, 60000); // 60000 밀리초 = 1분
 
     postButton.addEventListener("click", function (event) {
         event.preventDefault();
@@ -35,15 +19,14 @@ document.addEventListener('DOMContentLoaded', function () {
         })
             .then(response => {
                 if (response.ok) {
-                    alert('게시물이 등록되었습니다.');
+                    alert('신고가 완료되었습니다.');
                     window.location.href = '/reports/write';
                 } else {
-                    alert('게시물 등록에 실패했습니다.');
+                    alert('신고에 실패했습니다.');
                 }
             })
             .catch(error => {
                 console.error('Network error:', error);
             });
     });
-
 });
