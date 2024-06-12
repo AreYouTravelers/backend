229bff3be35bc0facb692427119fec3004694ea6
yezy <iyeahji@gmail.com>
2024-06-12 22:16:38 +0900

    Chore: Board Domain Exception

---
+++ src/main/java/com/example/domain/boards/exception/BoardNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.domain.boards.exception;
+
+import com.example.global.exception.BoardErrorCode;
+import com.example.global.exception.TravelersException;
+
+public class BoardNotFoundException extends TravelersException {
+    public BoardNotFoundException() {
+        super(BoardErrorCode.BOARD_NOT_FOUND);
+    }
+}
