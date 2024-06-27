21e9d6e4e13d9088837da5981e9c196eeb9ce7b3
yezy <82196217+yezyaa@users.noreply.github.com>
2024-06-27 13:18:27 +0900

    Chore: Docker 컨테이너 PID 상태 로그 기록
    - 현재 실행 중인 Docker 컨테이너 PID를 로그에 기록
    - 각 컨테이너를 중지 및 제거하는 과정 로그에 추가
    - 새로운 Docker 컨테이너 시작 로그 추가

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,24 @@ APP_LOG="$PROJECT_ROOT/log/application.log"
 ERROR_LOG="$PROJECT_ROOT/log/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/log/deploy.log"
 
-echo "> 현재 실행 중인 Docker 컨테이너 pid 확인했습니다." >> "$DEPLOY_LOG"
+echo "[$(date)] > 현재 실행 중인 Docker 컨테이너 PID 확인했습니다." >> "$DEPLOY_LOG"
 CURRENT_PIDS=$(sudo docker container ls -q)
 
 if [ -z "$CURRENT_PIDS" ]; then
-  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> "$DEPLOY_LOG"
+  echo "[$(date)] > 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> "$DEPLOY_LOG"
 else
+  echo "[$(date)] > 현재 구동중인 Docker 컨테이너 PID: $CURRENT_PIDS" >> "$DEPLOY_LOG"
   for PID in $CURRENT_PIDS
   do
-    echo "> docker stop $PID" >> "$DEPLOY_LOG" # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
+    echo "[$(date)] > docker stop $PID" >> "$DEPLOY_LOG" # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
     sudo docker stop $PID
+    echo "[$(date)] > docker rm $PID" >> "$DEPLOY_LOG"
     sudo docker rm $PID
   done
   sleep 5
 fi
+echo "[$(date)] > 모든 Docker 컨테이너가 중지 및 제거되었습니다." >> "$DEPLOY_LOG"
 
 cd /home/ubuntu/deploy
 sudo docker-compose -f docker-compose.yml up
+echo "[$(date)] > 새로운 Docker 컨테이너가 시작되었습니다." >> "$DEPLOY_LOG"
