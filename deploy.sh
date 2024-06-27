79707a3fc80d955eaecf0c1ec1a2594338c635e1
yezy <82196217+yezyaa@users.noreply.github.com>
2024-06-27 13:05:32 +0900

    Chore: 도커에서 SpringBoot와 Redis 컨테이너를 동시에 관리하도록 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,19 @@ ERROR_LOG="$PROJECT_ROOT/log/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/log/deploy.log"
 
 echo "> 현재 실행 중인 Docker 컨테이너 pid 확인했습니다." >> "$DEPLOY_LOG"
-CURRENT_PID=$(docker container ls -q)
+CURRENT_PIDS=$(sudo docker container ls -q)
 
-if [ -z $CURRENT_PID ];
-then
+if [ -z "$CURRENT_PIDS" ]; then
   echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> "$DEPLOY_LOG"
 else
-  echo "> docker stop $CURRENT_PID" >> "$DEPLOY_LOG" # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
-  docker stop $CURRENT_PID
-  docker rm $CURRENT_PID
+  for PID in $CURRENT_PIDS
+  do
+    echo "> docker stop $PID" >> "$DEPLOY_LOG" # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
+    sudo docker stop $PID
+    sudo docker rm $PID
+  done
   sleep 5
 fi
 
 cd /home/ubuntu/deploy
-docker-compose -f docker-compose.yml up
+sudo docker-compose -f docker-compose.yml up
